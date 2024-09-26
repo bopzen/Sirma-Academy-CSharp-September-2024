34003731c30cb1dbdafdd7e88fdd61fa87b456aa
bopzen <boppp@abv.bg>
2024-09-26 08:30:17 +0300

    Added more solutions

---
+++ 02. Conditional Statements/28ClimateZoneIdentifier/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _28ClimateZoneIdentifier
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double latitude = double.Parse(Console.ReadLine());
+
+			if (Math.Abs(latitude) > 66.5)
+			{
+				Console.WriteLine("Arctic Zone");
+			}
+			else if (Math.Abs(latitude) >= 23.5)
+			{
+				Console.WriteLine("Temperate Zone");
+			}
+			else if (Math.Abs(latitude) > 0)
+			{
+				Console.WriteLine("Tropic Zone");
+			}
+			else if (latitude == 0)
+			{
+				Console.WriteLine("Equator");
+			}
+		}
+	}
+}
---
+++ 02. Conditional Statements/29ArchitecturalEraIdentifier/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace _29ArchitecturalEraIdentifier
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int year = int.Parse(Console.ReadLine());
+			string material = Console.ReadLine();
+
+			if (year < 500 && material == "stone")
+			{
+				Console.WriteLine("Ancient");
+			}
+			else if (500 <= year && year <= 1500 && material == "stone")
+			{
+				Console.WriteLine("Medieval");
+			}
+			else if (1500 <= year && year <= 1800 && material == "wood")
+			{
+				Console.WriteLine("Colonial");
+			}
+			else if (1800 <= year && year <= 1900 && material == "steel")
+			{
+				Console.WriteLine("Industrial");
+			}
+			else if (year > 1900 && material == "steel")
+			{
+				Console.WriteLine("Modern");
+			}
+			else
+			{
+				Console.WriteLine("Uncertain");
+			}
+		}
+	}
+}
