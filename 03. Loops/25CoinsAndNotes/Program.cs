abe9eb57dd89bec07f725dc1f0dfa91c97f7b78a
bopzen <boppp@abv.bg>
2024-09-30 17:07:49 +0300

    Added more solutions

---
+++ 03. Loops/25CoinsAndNotes/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _25CoinsAndNotes
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int oneLev = int.Parse(Console.ReadLine());
+			int twoLev = int.Parse(Console.ReadLine());
+			int fiveLev = int.Parse(Console.ReadLine());
+			int amount = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i <= oneLev; i++)
+			{
+				for (int j = 0; j <= twoLev; j++)
+				{
+					for(int k = 0; k <= fiveLev; k++)
+					{
+						if (i * 1 + j * 2 + k * 5 == amount)
+						{
+							Console.WriteLine($"{i} * 1 lv. + {j} * 2 lv. + {k} * 5 lv. = {amount} lv.");
+						}
+					}
+				}
+			}
+		}
+	}
+}
---
+++ 03. Loops/26EvenPairs/Program.cs
@@ -0,0 +1,12 @@
+﻿namespace _26EvenPairs
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int firstPairStart = int.Parse(Console.ReadLine());
+			int secondPairStart = int.Parse(Console.ReadLine());
+
+		}
+	}
+}
