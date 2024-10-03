d59ff7ff9dd5c941f5a449b76801a9a40f4f1ba8
bopzen <boppp@abv.bg>
2024-10-03 11:53:16 +0300

    Added more solutions

---
+++ 03. Loops/29UniqueCodes/Program.cs
@@ -0,0 +1,41 @@
+﻿namespace _29UniqueCodes
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int firstLimit = int.Parse(Console.ReadLine());
+			int secondLimit = int.Parse(Console.ReadLine());
+			int thirdLimit = int.Parse(Console.ReadLine());
+
+			for (int i = 2; i <= firstLimit; i++)
+			{
+				for (int j = 2; j <= secondLimit; j++)
+				{
+					for (int k = 2; k <= thirdLimit; k++)
+					{
+						if ((i % 2 == 0) && CheckIfPrime(j) && (k % 2 == 0)) {
+							Console.WriteLine($"{i}{j}{k}");
+						}
+					}
+				}
+			}
+		}
+
+		public static bool CheckIfPrime(int number)
+		{
+			if (number <= 3)
+			{
+				return true;
+			}
+			for (int i = 2; i < number - 1; i++)
+			{
+				if (number % i == 0)
+				{
+					return false;
+				}
+			}
+			return true;
+		}
+	}
+}
---
+++ 03. Loops/31Half-RhombusFromAsterisks/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _31Half_RhombusFromAsterisks
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			for (int i = 0; i < n; i++)
+			{
+				for (int j = 0; j <= i; j++)
+				{
+					Console.Write("* ");
+				}
+				Console.WriteLine();
+			}
+			for (int i = n-2; i >= 0; i--)
+			{
+				for (int j = 0; j <= i; j++)
+				{
+					Console.Write("* ");
+				}
+				Console.WriteLine();
+			}
+		}
+	}
+}
