c6bf454f780d9ff5591a19270b1be33365095253
bopzen <boppp@abv.bg>
2024-10-03 09:42:04 +0300

    Added more solutions

--- 03. Loops/26EvenPairs/Program.cs
+++ 03. Loops/26EvenPairs/Program.cs
@@ -1,4 +1,7 @@
-﻿namespace _26EvenPairs
+﻿using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
+
+namespace _26EvenPairs
 {
 	internal class Program
 	{
@@ -6,7 +9,37 @@
 		{
 			int firstPairStart = int.Parse(Console.ReadLine());
 			int secondPairStart = int.Parse(Console.ReadLine());
+			int firstPairDiff = int.Parse(Console.ReadLine());
+			int secondPairDiff = int.Parse(Console.ReadLine());
 
+			for (int i = firstPairStart; i <= firstPairStart + firstPairDiff; i++)
+			{
+				for (int j = secondPairStart; j <= secondPairStart + secondPairDiff; j++)
+				{
+					if (CheckIfPrime(i) && CheckIfPrime(j)) 
+					{
+						Console.WriteLine($"{i}{j}");
+					}		
+				}
+			}
+
+
+		}
+
+		public static bool CheckIfPrime(int number)
+		{
+			if (number <=3)
+			{
+				return true;
+			}
+			for (int i = 2; i < number-1 ; i++)
+			{
+				if (number % i == 0)
+				{
+					return false;
+				}
+			}
+			return true;
 		}
 	}
 }
---
+++ 03. Loops/27Change/Program.cs
@@ -0,0 +1,58 @@
+﻿namespace _27Change
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double change = double.Parse(Console.ReadLine());
+			int count = 0;
+
+			int amount = (int)(change * 100);
+
+			while (amount > 0)
+			{
+				if (amount >= 200)
+				{
+					count += amount / 200;
+					amount %= 200;
+				}
+				else if (amount >= 100)
+				{
+					count += amount / 100;
+					amount %= 100;
+				}
+                else if (amount >= 50)
+                {
+					count += amount / 50;
+					amount %= 50;
+				}
+				else if (amount >= 20)
+				{
+					count += amount / 20;
+					amount %= 20;
+				}
+				else if (amount >= 10)
+				{
+					count += amount / 10;
+					amount %= 10;
+				}
+				else if (amount >= 5)
+				{
+					count += amount / 5;
+					amount %= 5;
+				}
+				else if (amount >= 2)
+				{
+					count += amount / 2;
+					amount %= 2;
+				}
+				else if (amount >= 1)
+				{
+					count += amount / 1;
+					amount %= 1;
+				}
+			}
+			Console.WriteLine(count);
+		}
+	}
+}
