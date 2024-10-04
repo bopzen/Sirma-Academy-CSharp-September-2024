36559f732f09d07e7f4b59f6fda580ff103f2967
bopzen <boppp@abv.bg>
2024-10-04 16:58:35 +0300

    Added more solutions

---
+++ 03. Loops/33TreePattern/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace _33TreePattern
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int n = int.Parse(Console.ReadLine());
+			for (int r = 1; r <= n; r++)
+			{
+				for (int c = 1; c <= n - r; c++)
+				{
+					Console.Write(" ");
+				}
+				for (int c = 1; c <= 2 * r - 1; c++)
+				{
+					Console.Write("*");
+				}
+				Console.WriteLine();
+			}
+			for (int c = 1; c <= n - 1; c++)
+			{
+				Console.Write(" ");
+			}
+			Console.WriteLine("*");
+		}
+	}
+}
