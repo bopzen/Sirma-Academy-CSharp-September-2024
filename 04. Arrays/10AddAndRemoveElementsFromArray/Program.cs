ad20dfc645ea8320ba8c9001090a49e170cee286
bopzen <boppp@abv.bg>
2024-10-04 23:17:14 +0300

    Added more soluitons

---
+++ 04. Arrays/10AddAndRemoveElementsFromArray/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace _10AddAndRemoveElementsFromArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
