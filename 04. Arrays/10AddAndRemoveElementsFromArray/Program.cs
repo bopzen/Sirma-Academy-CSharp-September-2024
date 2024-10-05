a5218c203b5617d8a5c4f23b87c3ebf2b4cb2e5b
bopzen <boppp@abv.bg>
2024-10-05 22:04:44 +0300

    Added more solutions

--- 04. Arrays/10AddAndRemoveElementsFromArray/Program.cs
+++ 04. Arrays/10AddAndRemoveElementsFromArray/Program.cs
@@ -4,7 +4,34 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            string[] commands = Console.ReadLine().Split(",");
+            int[] numbers = new int[commands.Length];
+            int i = 0;
+            int currentNumber = 1;
+            foreach (string command in commands)
+            {
+                if (command == "add")
+                {
+                    numbers[i++] = currentNumber++;
+                }
+                else if (command == "remove")
+                {
+                    currentNumber++;
+                }
+            }
+            bool isEmpty = true;
+            foreach (int number in numbers)
+            {
+                if (number > 0)
+                {
+                    isEmpty = false;
+                    Console.Write(number + " ");
+                }
+            }
+            if (isEmpty)
+            {
+                Console.WriteLine("Empty");
+            }
         }
     }
 }
