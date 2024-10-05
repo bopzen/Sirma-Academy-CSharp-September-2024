1d1730b880fc2a7d8fa7ddc6f1c245fc3aa266ff
bopzen <boppp@abv.bg>
2024-10-05 23:21:34 +0300

    Added more solutions

---
+++ 04. Arrays/12ExtractANon-DecreasingSubsequenceFromAnArray/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace _12ExtractANon_DecreasingSubsequenceFromAnArray
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            int maxNumber = 0;
+            foreach (int number in numbers)
+            {
+                if (number >= maxNumber)
+                {
+                    Console.Write(number + " ");
+                    maxNumber = number;
+                }
+            }
+
+        }
+    }
+}
---
+++ 04. Arrays/16ProcessOddNumbers/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _16ProcessOddNumbers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] array = Console.ReadLine().Split(", ").Select(int.Parse).ToArray();
+            for (int i = array.Length-1; i >= 0; i--)
+            {
+                if (i % 2 != 0)
+                {
+                    Console.Write(array[i] * 2 + " ");
+                }
+            }
+
+        }
+    }
+}
