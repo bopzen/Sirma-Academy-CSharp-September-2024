dbb20b723ee9f9de66b9d9a3f5df6b6d988beffe
bopzen <boppp@abv.bg>
2024-10-11 22:54:49 +0300

    Added more solutions

---
+++ 07. Stack and Queue/12TaskScheduler/Program.cs
@@ -0,0 +1,35 @@
+﻿
+namespace _12TaskScheduler
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue<string, int> queue = new PriorityQueue<string, int>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end")
+                {
+                    break;
+                }
+                if (input == "getNextTask")
+                {
+                    Console.WriteLine(GetNextTask(queue));
+                }
+                else
+                {
+                    string[] tokens = input.Split(" ");
+                    string task = tokens[1];
+                    int priority = int.Parse(tokens[2]);
+                    queue.Enqueue(task, -priority);
+                }
+            }
+        }
+
+        private static string GetNextTask(PriorityQueue<string, int> queue)
+        {
+            return queue.Peek();
+        }
+    }
+}
