3f7e1411389fc76d7075bb7c96ae7d6daae1384a
bopzen <boppp@abv.bg>
2024-10-11 08:45:06 +0300

    Added more solutions

---
+++ 07. Stack and Queue/01ReverseNumbersStack/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace _01ReverseNumbersStack
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			int[] numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+
+			Stack<int> stack = new Stack<int>();
+			foreach (var number in numbers)
+			{
+				stack.Push(number);
+			}
+
+			while (stack.Count > 0)
+			{
+				Console.Write(stack.Pop()+ " ");
+			}
+		}
+	}
+}
