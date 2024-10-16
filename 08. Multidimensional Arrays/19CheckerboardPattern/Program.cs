8c8d3d94f08cfbd006d2b2ba7ca89b9f9ad9e2fa
bopzen <boppp@abv.bg>
2024-10-16 21:37:11 +0300

    Added more solutions

---
+++ 08. Multidimensional Arrays/19CheckerboardPattern/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace _19CheckerboardPattern
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[n,n];
+            int num = 0;
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    matrix[i, j] = num;
+                    if (num == 0)
+                    {
+                        if (j == n-1 && n % 2 == 0)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            num = 1;
+                        }
+                        
+                    }
+                    else if (num == 1)
+                    {
+                        if (j == n - 1 && n % 2 == 0)
+                        {
+                            continue;
+                        }
+                        else
+                        {
+                            num = 0;
+                        }
+                    }
+                }
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < n; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
