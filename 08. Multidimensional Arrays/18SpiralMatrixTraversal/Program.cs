1328a13567463d87c16d5e5e3f40d5c022c5d4d2
bopzen <boppp@abv.bg>
2024-10-16 22:14:58 +0300

    Added more solutions

--- 08. Multidimensional Arrays/18SpiralMatrixTraversal/Program.cs
+++ 08. Multidimensional Arrays/18SpiralMatrixTraversal/Program.cs
@@ -8,21 +8,41 @@
 			int m = size[0];
 			int n = size[1];
 			int[,] matrix = new int[m, n];
+			List<int> spiral = new List<int>();
 
 			for (int i = 0; i < m; i++)
 			{
-				int[] row = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
+				int[] r = Console.ReadLine().Split(" ").Select(int.Parse).ToArray();
 				for (int j = 0; j < n; j++)
 				{
-					matrix[i, j] = row[j];
+					matrix[i, j] = r[j];
 				}
 			}
 
 			int offset = 0;
-			for (int i = 0; i < m - offset; i++)
+			int row = 0;
+			int col = 0;
+			for (int count = 0; count < m * n; count++)
 			{
-
-			}
+                for (col = 0 + offset; col < n - offset; col++)
+                {
+					spiral.Add(matrix[0 + offset, col]);
+                }
+				for (row = 1 + offset; row < m - offset; row++)
+				{
+                    spiral.Add(matrix[row, n-1-offset]);
+                }
+				for (col = n - offset -2; col >= 0 + offset; col--)
+				{
+                    spiral.Add(matrix[m-offset-1, col]);
+                }
+				for (row = m - offset -2 ; row >= 1 + offset; row--)
+				{
+                    spiral.Add(matrix[row, 0 + offset]);
+                }
+				offset++;
+            }
+			Console.WriteLine(string.Join(" ", spiral));
 		}
 	}
 }
