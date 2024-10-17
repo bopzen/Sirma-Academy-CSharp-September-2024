8874829324363179c74dc2c2f6ac86d66f17c7c8
bopzen <boppp@abv.bg>
2024-10-17 14:31:26 +0300

    Added more solutions

--- 08. Multidimensional Arrays/20MaximalSum/Program.cs
+++ 08. Multidimensional Arrays/20MaximalSum/Program.cs
@@ -8,6 +8,9 @@
             int m = size[0];
             int n = size[1];
             int[,] matrix = new int[m, n];
+            int[,] maxMatrix = new int[3, 3];
+            
+            int maxSum = 0;
 
             for (int i = 0; i < m; i++)
             {
@@ -17,6 +20,46 @@
                     matrix[i, j] = row[j];
                 }
             }
-        }
+
+			for (int i = 0; i < m - 2; i++)
+			{
+				
+				for (int j = 0; j < n - 2; j++)
+				{
+					int sum = 0;
+					int[,] subMatrix = new int[3, 3];
+					for (int k = i; k <= i+2; k++) 
+                    { 
+                        for(int k2 = j; k2 <= j+2; k2++)
+                        {
+							subMatrix[k-i, k2-j] = matrix[k, k2];
+                            sum += matrix[k, k2];
+
+						}
+                    }
+					if (sum > maxSum)
+					{
+						maxSum = sum;
+						for (int k = 0; k < 3; k++)
+                        {
+                            for (int k2 = 0; k2 < 3; k2++)
+                            {
+                                maxMatrix[k, k2] = subMatrix[k, k2];
+                            }
+                        }
+					}
+				}
+			}
+            Console.WriteLine("Sum = " + maxSum);
+            for (int i = 0; i < 3; i++)
+            {
+                for (int j = 0; j < 3; j++)
+                {
+                    Console.Write(maxMatrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+
+		}
     }
 }
