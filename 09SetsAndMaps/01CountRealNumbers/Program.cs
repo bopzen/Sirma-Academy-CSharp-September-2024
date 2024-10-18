ffa78b6df794a3ea944b7228877a556563e61d56
bopzen <boppp@abv.bg>
2024-10-18 09:06:13 +0300

    Added more solutions

---
+++ 09SetsAndMaps/01CountRealNumbers/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace _01CountRealNumbers
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			double[] numbers = Console.ReadLine().Split(" ").Select(double.Parse).ToArray();
+			Dictionary<double, int> counts = new Dictionary<double, int>();
+			foreach (double number in numbers)
+			{
+				if (counts.ContainsKey(number))
+				{
+					counts[number]++;
+				}
+				else
+				{
+					counts[number] = 0;
+					counts[number]++;
+				}
+			}
+
+			foreach (var kvp in counts) 
+			{
+				Console.WriteLine($"{kvp.Key} -> {kvp.Value}");
+			}
+		}
+	}
+}
