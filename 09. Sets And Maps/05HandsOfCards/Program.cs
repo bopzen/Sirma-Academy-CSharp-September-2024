bbcfe5daac99518bb50474c60189be2f3684e22f
bopzen <boppp@abv.bg>
2024-10-19 12:53:08 +0300

    Added more solutions

---
+++ 09. Sets And Maps/05HandsOfCards/Program.cs
@@ -0,0 +1,72 @@
+﻿namespace _05HandsOfCards
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> players = new Dictionary<string, List<string>>();
+            Dictionary<string, int> types = new Dictionary<string, int>()
+            {
+                {"S", 4},
+                {"H", 3},
+                {"D", 2},
+                {"C", 1},
+            };
+            Dictionary<string, int> powers = new Dictionary<string, int>()
+            {
+                {"J", 11},
+                {"Q", 12},
+                {"K", 13},
+                {"A", 14},
+            };
+            while (true)
+            {
+                string input = Console.ReadLine();
+
+                if (input == "JOKER")
+                {
+                    break;
+                }
+                string[] tokens = input.Split(": ");
+                string name = tokens[0];
+                string[] cards = tokens[1].Split(", ");
+                foreach (string card in cards)
+                {
+                    if (players.ContainsKey(name))
+                    {
+                        if (!players[name].Contains(card))
+                        {
+                            players[name].Add(card);
+                        }
+                    }
+                    else
+                    {
+                        players[name] = new List<string>();
+                        players[name].Add(card);
+                    }
+                }
+            }
+            foreach (var kvp in players)
+            {
+                int sum = 0;
+                foreach (string card in kvp.Value)
+                {
+                    int typeValue = types[card[card.Length - 1].ToString()];
+                    string power = card.Remove(card.Length - 1);
+                    int powerValue;
+                    if (powers.ContainsKey(power))
+                    {
+                        powerValue = powers[power];
+                    }
+                    else
+                    {
+                        powerValue = int.Parse(power);
+                    }
+
+                    sum += typeValue * powerValue;
+                }
+                Console.WriteLine($"{kvp.Key}: {sum}");
+            }
+        }
+    }
+}
---
+++ 09. Sets And Maps/06PopulationCounter/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _06PopulationCounter
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, Dictionary<string, int>> countryData = new Dictionary<string, Dictionary<string, int>>();
+            Dictionary<string, int> countryPopulation = new Dictionary<string, int>();
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "report")
+                {
+                    break;
+                }
+                string[] tokens = input.Split("|");
+                string city = tokens[0];
+                string country = tokens[1];
+                int population = int.Parse(tokens[2]);
+
+                if (!countryData.ContainsKey(country))
+                {
+                    countryData[country] = new Dictionary<string, int>();
+                    countryPopulation[country] = 0;                
+                }
+                countryData[country].Add(city, population);
+                countryPopulation[country] += population;
+
+            }
+
+            var countryPopulationSorted = countryPopulation.OrderByDescending(x => x.Value);
+            foreach (var country in countryPopulationSorted)
+            {
+                string countryName = country.Key;
+                int totalPopulation = country.Value;
+                Console.WriteLine($"{countryName} (total population: {totalPopulation})");
+
+                var countryDataSorted = countryData[countryName].OrderByDescending(x => x.Value);
+                foreach (var city in countryDataSorted)
+                {
+                    string cityName = city.Key;
+                    int cityPopulation = city.Value;
+                    Console.WriteLine($"=> {cityName}: {cityPopulation}");
+                }
+            }
+        }
+    }
+}
