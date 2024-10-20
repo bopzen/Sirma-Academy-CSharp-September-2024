cd6c3e3cb0786d21a568200636493d58395a3600
bopzen <boppp@abv.bg>
2024-10-20 22:45:17 +0300

    Added more solutions

--- 09. Sets And Maps/14StudentAcademy/Program.cs
+++ 09. Sets And Maps/14StudentAcademy/Program.cs
@@ -4,7 +4,33 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int n = int.Parse(Console.ReadLine());
+            Dictionary<string, List<double>> students = new Dictionary<string, List<double>>();
+            string name = "";
+            for (int i = 0; i < 2 * n; i++)
+            {
+                string input = Console.ReadLine();
+                if (i % 2 == 0)
+                {
+                    name = input;
+                }
+                else
+                {
+                    if (!students.ContainsKey(name))
+                    {
+                        students[name] = new List<double>();
+                    }
+                    students[name].Add(double.Parse(input));
+                }
+            }
+            foreach (var kvp in students)
+            {
+                if (kvp.Value.Average() >= 4.50)
+                {
+                    Console.WriteLine($"{kvp.Key} -> {kvp.Value.Average():F2}");
+                }
+            }
+
         }
     }
 }
---
+++ 09. Sets And Maps/15CompanyUsers/Program.cs
@@ -0,0 +1,40 @@
+﻿namespace _15CompanyUsers
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, List<string>> companies = new Dictionary<string, List<string>>();
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "End")
+                {
+                    break;
+                }
+                string[] tokens = input.Split(" -> ");
+                string company = tokens[0];
+                string employeeId = tokens[1];
+
+                if (!companies.ContainsKey(company))
+                {
+                    companies[company] = new List<string>();
+                }
+                if (!companies[company].Contains(employeeId))
+                {
+                    companies[company].Add(employeeId);
+                }
+            }
+
+            foreach (var kvp in companies)
+            {
+                Console.WriteLine(kvp.Key);
+                foreach (var employeeId in kvp.Value)
+                {
+                    Console.WriteLine($"-- {employeeId}");
+                }
+            }
+
+        }
+    }
+}
