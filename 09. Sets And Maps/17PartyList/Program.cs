54a07ec3067851b85e6a1bbaad85415a41dad4e2
bopzen <boppp@abv.bg>
2024-10-21 16:49:02 +0300

    Added more solutions

---
+++ 09. Sets And Maps/17PartyList/Program.cs
@@ -0,0 +1,47 @@
+﻿namespace _17PartyList
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			HashSet<string> vipGuests = new HashSet<string>();
+			HashSet<string> regularGuests = new HashSet<string>();
+			HashSet<string> attended = new HashSet<string>();
+			while (true)
+			{
+				string input = Console.ReadLine();
+				if (input == "PARTY")
+				{
+					break;
+				}
+
+				if (char.IsDigit(input[0]))
+				{
+					vipGuests.Add(input);
+				}
+				else
+				{
+					regularGuests.Add(input);
+				}
+			}
+
+			while (true)
+			{
+				string input = Console.ReadLine();
+				if (input == "END")
+				{
+					break;
+				}
+
+				vipGuests.Remove(input);
+				regularGuests.Remove(input);
+			}
+
+			Console.WriteLine(vipGuests.Union(regularGuests).Count());
+			foreach (string guest in vipGuests.Union(regularGuests))
+			{
+				Console.WriteLine(guest);
+			}
+		}
+	}
+}
---
+++ 09. Sets And Maps/18WarGame/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace _18WarGame
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			HashSet<int> playerOne = Console.ReadLine().Split(" ").Select(int.Parse).ToHashSet();
+			HashSet<int> playerTwo = Console.ReadLine().Split(" ").Select(int.Parse).ToHashSet();
+
+			for (int i = 0; i < 50; i++)
+			{
+
+
+				if (playerOne.Count() == 0)
+				{
+					break;
+				}
+				if (playerTwo.Count() == 0)
+				{
+					break;
+				}
+			}
+		}
+	}
+}
