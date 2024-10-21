07c14458a0b69377ac16e240a9df2aad2d01c829
bopzen <boppp@abv.bg>
2024-10-21 16:08:06 +0300

    Added more solutions

--- 09. Sets And Maps/16ParkingLot/Program.cs
+++ 09. Sets And Maps/16ParkingLot/Program.cs
@@ -4,7 +4,37 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("test")
+			HashSet<string> carsInParkingLot = new HashSet<string>();
+			while (true)
+			{
+				string input = Console.ReadLine();
+				if (input == "END")
+				{
+					break;
+				}
+
+				string[] tokens = input.Split(", ");
+				string direction = tokens[0];
+				string carNumber = tokens[1];
+
+				if (direction == "IN")
+				{
+					carsInParkingLot.Add(carNumber);
+				}
+				else if (direction == "OUT")
+				{
+					carsInParkingLot.Remove(carNumber);
+				}
+
+				if (carsInParkingLot.Count == 0)
+				{
+					Console.WriteLine("Parking Lot is Empty");
+				}
+			}
+			foreach (var carNumber in carsInParkingLot)
+			{
+				Console.WriteLine(carNumber);
+			}
 		}
 	}
 }
