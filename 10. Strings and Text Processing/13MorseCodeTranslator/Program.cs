254afaf4f6114c48150cc9f1785c379f5ab62cba
bopzen <boppp@abv.bg>
2024-10-23 22:01:45 +0300

    Added more solutions

---
+++ 10. Strings and Text Processing/13MorseCodeTranslator/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace _13MorseCodeTranslator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Dictionary<string, char> morseCodeLetters = new Dictionary<string, char>() 
+            {
+                { ".-", 'A' },
+                { "-...", 'B' },
+                { "-.-.", 'C' },
+                { "-..", 'D' },
+                { ".", 'E' },
+                { "..-.", 'F' },
+                { "--.", 'G' },
+                { "....", 'H' },
+                { "..", 'I' },
+                { ".---", 'J' },
+                { "-.-", 'K' },
+                { ".-..", 'L' },
+                { "--", 'M' },
+                { "-.", 'N' },
+                { "---", 'O' },
+                { ".--.", 'P' },
+                { "--.-", 'Q' },
+                { ".-.", 'R' },
+                { "...", 'S' },
+                { "-", 'T' },
+                { "..-", 'U' },
+                { "...-", 'V' },
+                { ".--", 'W' },
+                { "-..-", 'X' },
+                { "-.--", 'Y' },
+                { "--..", 'Z' }
+            };
+            string[] input = Console.ReadLine().Split(" | ");
+            foreach (string line in input)
+            {
+                string[] morseCode = line.Split(" ");
+                foreach (var code in morseCode)
+                {
+                    Console.Write(morseCodeLetters[code]);
+                }
+                Console.Write(" ");
+            }
+
+        }
+    }
+}
---
+++ 10. Strings and Text Processing/14HTML/Program.cs
@@ -0,0 +1,35 @@
+﻿using System.Text;
+
+namespace _14HTML
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            StringBuilder sb = new StringBuilder();
+
+            string title = Console.ReadLine();
+            sb.Append("<h1>\n");
+            sb.Append($"    {title}\n");
+            sb.Append("</h1>\n");
+
+            string article = Console.ReadLine();
+            sb.Append("<article>\n");
+            sb.Append($"    {article}\n");
+            sb.Append("</article>\n");
+
+            while (true)
+            {
+                string input = Console.ReadLine();
+                if (input == "end of comments")
+                {
+                    break;
+                }
+                sb.Append("<div>\n");
+                sb.Append($"    {input}\n");
+                sb.Append("</div>\n");
+            }
+            Console.WriteLine(sb.ToString());
+        }
+    }
+}
