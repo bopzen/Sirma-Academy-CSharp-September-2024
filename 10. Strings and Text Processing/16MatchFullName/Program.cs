6b44110b2696a905c08748d7a1129cd62145e0f8
bopzen <boppp@abv.bg>
2024-10-24 13:04:27 +0300

    Added more solutions

---
+++ 10. Strings and Text Processing/16MatchFullName/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _16MatchFullName
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			string pattern = @"\b[A-Z]{1}[a-z]+ [A-Z]{1}[a-z]+\b";
+			Regex regex = new Regex(pattern);
+			MatchCollection matches = regex.Matches(input);
+			foreach (Match match in matches)
+			{
+				Console.Write(match.ToString() + " ");
+			}
+		}
+	}
+}
---
+++ 10. Strings and Text Processing/17MatchPhoneNumber/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _17MatchPhoneNumber
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			string pattern = @"\+359([ -])\d{1}\1\d{3}\1\d{4}\b";
+			Regex regex = new Regex(pattern);
+			MatchCollection matches = regex.Matches(input);
+
+			Console.WriteLine(string.Join(", ", matches));
+		}
+	}
+}
