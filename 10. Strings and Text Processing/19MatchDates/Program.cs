dafef1ba693e3e0b4f2f1bbc4968b0d0c92f43da
bopzen <boppp@abv.bg>
2024-10-24 22:04:11 +0300

    Added more solutions

--- 10. Strings and Text Processing/19MatchDates/Program.cs
+++ 10. Strings and Text Processing/19MatchDates/Program.cs
@@ -7,10 +7,13 @@ namespace _19MatchDates
 		static void Main(string[] args)
 		{
 			string input = Console.ReadLine();
-			string pattern = @"(?<Day>\d{2})([.\-\/]{1})(?<Month>[A-z]{1}[a-z]{2})\2(?<Year>\d{4})";
+			string pattern = @"(?<Day>\d{2})([.\-\/]{1})(?<Month>[A-z]{1}[a-z]{2})\1(?<Year>\d{4})";
 			Regex regex = new Regex(pattern);
 			MatchCollection matches = regex.Matches(input);
-			Console.WriteLine(string.Join(", ", matches));
+			foreach (Match match in matches)
+			{
+				Console.WriteLine($"Day: {match.Groups["Day"].Value}, Month: {match.Groups["Month"].Value}, Year: {match.Groups["Year"].Value}");
+			}
 		}
 	}
 }
---
+++ 10. Strings and Text Processing/20StarBattlesEnigma/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace _20StarBattlesEnigma
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            List<string> decryptedMessages = new List<string>();
+
+            for (int i = 0; i < n; i++)
+            {
+                string encryptedMessage = Console.ReadLine();
+                int counter = 0;
+                foreach (var letter in encryptedMessage)
+                {
+                    char symbol = char.ToLower(letter);
+                    if (symbol == 's' || symbol == 't' || symbol == 'a' || symbol == 'r')
+                    {
+                        counter++;
+                    }
+                }
+                string decryptedMessage = "";
+                foreach (char letter in encryptedMessage)
+                {
+                    char newLetter = (char)((int)letter - counter);
+                    decryptedMessage += newLetter;
+                }
+                decryptedMessages.Add(decryptedMessage);
+            }
+            foreach (var message in decryptedMessages)
+            {
+                Console.WriteLine(message);
+            }
+        }
+    }
+}
