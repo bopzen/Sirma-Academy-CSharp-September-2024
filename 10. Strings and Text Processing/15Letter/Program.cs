cc890efb69b9b651746c4f05b9c34d6d4bb6cf6e
bopzen <boppp@abv.bg>
2024-10-24 10:44:57 +0300

    Added more solutions

--- 10. Strings and Text Processing/15Letter/Program.cs
+++ 10. Strings and Text Processing/15Letter/Program.cs
@@ -1,11 +1,30 @@
-﻿namespace _15Letter
+﻿using System.Text.RegularExpressions;
+
+namespace _15Letter
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string[] input = Console.ReadLine().Split(",");
+            string text = Console.ReadLine();
+            string[] words = Console.ReadLine().Split(", ");
+            string pattern = @"[_]+";
+            MatchCollection matches = Regex.Matches(text, pattern);
 
-        }
+			for (int i = matches.Count - 1; i >= 0; i--)
+			{
+				Match match = matches[i];
+
+				foreach (string word in words)
+                {
+                    if (word.Length == match.Length)
+                    {
+                        text = text.Substring(0, match.Index) + word + text.Substring(match.Index + match.Length);
+                        break;
+                    }
+                }
+            }
+            Console.WriteLine(text);
+		}
     }
 }
