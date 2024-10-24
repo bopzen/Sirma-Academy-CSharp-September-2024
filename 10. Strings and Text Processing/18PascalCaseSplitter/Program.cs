994bcdab540bf597f1bc30d6a5c6fbc897ef6f06
bopzen <boppp@abv.bg>
2024-10-24 16:57:19 +0300

    Added more solutions

---
+++ 10. Strings and Text Processing/18PascalCaseSplitter/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _18PascalCaseSplitter
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			string pattern = @"[A-Z]{1}[a-z]+";
+			Regex regex = new Regex(pattern);
+			MatchCollection matches = regex.Matches(input);
+			Console.WriteLine(string.Join(", ", matches));
+		}
+	}
+}
---
+++ 10. Strings and Text Processing/19MatchDates/Program.cs
@@ -0,0 +1,16 @@
+﻿using System.Text.RegularExpressions;
+
+namespace _19MatchDates
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string input = Console.ReadLine();
+			string pattern = @"(?<Day>\d{2})([.\-\/]{1})(?<Month>[A-z]{1}[a-z]{2})\2(?<Year>\d{4})";
+			Regex regex = new Regex(pattern);
+			MatchCollection matches = regex.Matches(input);
+			Console.WriteLine(string.Join(", ", matches));
+		}
+	}
+}
