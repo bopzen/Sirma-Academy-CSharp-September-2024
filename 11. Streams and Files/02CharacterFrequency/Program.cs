66d69a9c8f98f8391dc305ddcd0947cefbf8a6a2
bopzen <boppp@abv.bg>
2024-11-13 16:49:12 +0200

    Added more solutions

---
+++ 11. Streams and Files/02CharacterFrequency/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace _02CharacterFrequency
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string filePath = @"..\..\..\input.txt";
+			Dictionary<char, int> frequency = new Dictionary<char, int>();
+
+			using (StreamReader reader = new StreamReader(filePath))
+			{
+				int symbol;
+				while((symbol = reader.Read()) != -1)
+				{
+					if (!frequency.ContainsKey((char)symbol))
+					{
+						frequency[(char)symbol] = 0;
+					}
+					frequency[(char)symbol]++;
+				}
+			}
+
+			foreach (var kvp in frequency)
+			{
+				Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+			}
+		}
+	}
+}
