db4227aef108c2ea18cfbfc3fae1177dbd9050e5
bopzen <boppp@abv.bg>
2024-11-13 21:59:32 +0200

    Added more solutions

---
+++ 11. Streams and Files/03ReplaceWord/Program.cs
@@ -0,0 +1,33 @@
+﻿using System.Text;
+
+namespace _03ReplaceWord
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string fileInputPath = @"..\..\..\input.txt";
+            string fileOutputPath = @"..\..\..\output.txt";
+            StringBuilder sb = new StringBuilder();
+
+            using (StreamReader reader = new StreamReader(fileInputPath))
+            {
+                string line = reader.ReadLine();
+                string[] replaceText = line.Split(" -> ");
+                string oldText = replaceText[0];
+                string newText = replaceText[1];
+
+                while ((line = reader.ReadLine()) != null)
+                {
+                    line = line.Replace(oldText, newText);
+                    sb.Append(line);
+                }
+            }
+            using (FileStream stream = new FileStream(fileOutputPath, FileMode.Create, FileAccess.Write))
+            {
+                byte[] data = Encoding.UTF8.GetBytes(sb.ToString());
+                stream.Write(data, 0, data.Length);
+            }
+        }
+    }
+}
---
+++ 11. Streams and Files/04CountLinesWordsandCharacters/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace _04CountLinesWordsandCharacters
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string filePath = @"..\..\..\input.txt";
+            int linesCount = 0;
+            int wordsCount = 0;
+            int charactersCount = 0;
+            using (StreamReader reader = new StreamReader(filePath))
+            {
+                string line;
+                while ((line = reader.ReadLine()) != null)
+                {
+                    linesCount++;
+                    wordsCount += line.Split(" ").ToArray().Length;
+                    charactersCount += line.Length;
+                }
+            }
+            Console.WriteLine(linesCount);
+            Console.WriteLine(wordsCount);
+            Console.WriteLine(charactersCount);
+        }
+    }
+}
