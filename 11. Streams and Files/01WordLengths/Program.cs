e56c789f2ddfc94c85fd63a2d7579f16831d7f57
bopzen <boppp@abv.bg>
2024-11-13 16:36:40 +0200

    Added more solutions

--- 11. Streams and Files/01WordLengths/Program.cs
+++ 11. Streams and Files/01WordLengths/Program.cs
@@ -4,7 +4,19 @@
 	{
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+			string filePath = @"..\..\..\input.txt";
+			List<string> words = new List<string>();
+
+			using (StreamReader reader = new StreamReader(filePath))
+			{
+				string line;
+				while ((line = reader.ReadLine()) != null)
+				{
+					words.AddRange(line.Split(" "));
+				}
+			}
+
+			Console.WriteLine(string.Join(", ", words.Select(w => w.Length)));
 		}
 	}
 }
