dd6aab0885fd03bc6d3834a14e385facd6e5270b
bopzen <boppp@abv.bg>
2024-11-14 09:31:46 +0200

    Added more solutions

---
+++ 11. Streams and Files/05ExtractFile/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace _05ExtractFile
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string filePath = @"..\..\..\presentation.pptx";
+
+			string fileName = Path.GetFileNameWithoutExtension(filePath);
+			string fileExtension = Path.GetExtension(filePath);
+			Console.WriteLine($"File name: {fileName}");
+			Console.WriteLine($"File extension: {fileExtension}");
+
+			FileInfo fileInfo = new FileInfo(filePath);
+			Console.WriteLine($"File size: {fileInfo.Length} bytes");
+		}
+	}
+}
---
+++ 11. Streams and Files/06ReverseLines/Program.cs
@@ -0,0 +1,29 @@
+﻿using System.Text;
+
+namespace _06ReverseLines
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			string fileInputPath = @"..\..\..\input.txt";
+			string fileOutputPath = @"..\..\..\output.txt";
+
+			StringBuilder sb = new StringBuilder();
+
+			using (StreamReader reader = new StreamReader(fileInputPath))
+			{
+				string line;
+				while ((line = reader.ReadLine()) != null)
+				{
+					sb.AppendLine(new string(line.ToCharArray().Reverse().ToArray()));
+				}
+			}
+
+			using (StreamWriter writer = new StreamWriter(fileOutputPath))
+			{
+				writer.WriteLine(sb.ToString());
+			}
+		}
+	}
+}
