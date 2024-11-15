7db359b8feef1e9a28de68aafb8890adf231bed6
bopzen <boppp@abv.bg>
2024-11-15 10:11:22 +0200

    Added more solutions

--- 11. Streams and Files/11SerializeAndDeserializeMap/Program.cs
+++ 11. Streams and Files/11SerializeAndDeserializeMap/Program.cs
@@ -18,6 +18,13 @@ namespace _11SerializeAndDeserializeMap
 
 			SaveMapToFile(map, filePath);
 
+			Dictionary<string, int> loadedMap = LoadMapFromFile(filePath);
+
+			foreach (var kvp in loadedMap)
+			{
+				Console.WriteLine($"{kvp.Key}: {kvp.Value}");
+			}
+
 			static void SaveMapToFile(Dictionary<string, int> map, string filePath)
 			{
 				using (FileStream fs = new FileStream(filePath, FileMode.Create))
@@ -31,6 +38,26 @@ namespace _11SerializeAndDeserializeMap
 						writer.Write(kvp.Value);
 					}
 				}
+			}
+
+			static Dictionary<string, int> LoadMapFromFile(string filePath)
+			{
+				Dictionary<string, int> map = new Dictionary<string, int>();
+
+				using (FileStream fs = new FileStream(filePath, FileMode.Open))
+				using (BinaryReader reader = new BinaryReader(fs))
+				{
+					int count = reader.ReadInt32();
+
+					for (int i = 0; i < count; i++)
+					{
+						string key = reader.ReadString();
+						int value = reader.ReadInt32();
+						map[key] = value;
+					}
+				}
+
+				return map;
 			}
 		}
 	}
