a488788ca44df3ee8e1ceaa739dae24f76ab08b5
bopzen <boppp@abv.bg>
2024-11-25 22:04:46 +0200

    Added Interfaces

---
+++ 15. Inheritance, Interfaces and Abstraction/InventoryManagementSystem/Contracts/ISellable.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventoryManagementSystem.Contracts
+{
+    public interface ISellable
+    {
+        void setPrice(double price);
+        double getPrice();
+
+    }
+}
