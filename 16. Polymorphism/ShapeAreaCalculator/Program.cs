ab8689afa581466a84ab4b3b792bdfbec6317a18
bopzen <boppp@abv.bg>
2024-11-28 09:48:31 +0200

    Added all shapes

---
+++ 16. Polymorphism/ShapeAreaCalculator/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace ShapeAreaCalculator
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Shape> shapes = new List<Shape>
+            {
+                new Circle(5),
+                new Rectangle(4, 6),
+                new Triangle(5, 8)
+            };
+
+            foreach (Shape shape in shapes)
+            {
+                Console.WriteLine($"{shape.GetType().Name} has area {shape.GetArea():F2}");
+            }
+        }
+    }
+}
---
+++ 16. Polymorphism/ShapeAreaCalculator/Triangle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ShapeAreaCalculator
+{
+    public class Triangle : Shape
+    {
+        public double Base {  get; set; }
+        public double Height { get; set; }
+
+        public Triangle(double baseSide, double height) 
+        {
+            Base = baseSide;
+            Height = height;
+        }
+
+        public override double GetArea()
+        {
+            return Base * Height / 2;
+        }
+    }
+}
