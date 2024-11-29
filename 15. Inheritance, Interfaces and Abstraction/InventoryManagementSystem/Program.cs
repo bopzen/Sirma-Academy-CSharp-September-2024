a4e835cac43deb9b2ba4a2192d03e7edbeb96c78
bopzen <boppp@abv.bg>
2024-11-29 14:44:23 +0200

    Updated comments

--- 15. Inheritance, Interfaces and Abstraction/InventoryManagementSystem/Program.cs
+++ 15. Inheritance, Interfaces and Abstraction/InventoryManagementSystem/Program.cs
@@ -4,7 +4,7 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //TODO the rest of the task when I have more time
         }
     }
 }
