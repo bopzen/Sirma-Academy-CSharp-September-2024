63548bd467da32b084b2845514599d1203e00fac
bopzen <boppp@abv.bg>
2024-12-06 14:38:38 +0200

    Added Library class

---
+++ 18. Iterators And Comparators/LibraryBooks/Library.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBooks
+{
+    public class Library : IEnumerable<Book>
+    {
+        private List<Book> books;
+
+        public Library(params Book[] books)
+        {
+            this.books = new List<Book>(books);
+        }
+
+        public IEnumerator<Book> GetEnumerator()
+        {
+            return new LibraryIterator(books);
+        }
+
+        IEnumerator IEnumerable.GetEnumerator()
+        {
+            return this.GetEnumerator();
+        }
+
+        private class LibraryIterator : IEnumerator<Book>
+        {
+            private readonly List<Book> books;
+            private int currentIndex;
+
+            public LibraryIterator(List<Book> books)
+            {
+                this.books = books;
+                this.Reset();
+            }
+            public Book Current => books[currentIndex];
+
+            object IEnumerator.Current => Current;
+
+            public void Dispose() {}
+
+            public bool MoveNext() => ++currentIndex < books.Count;
+
+            public void Reset()
+            {
+                currentIndex = -1;
+            }
+        }
+
+    }
+
+}
--- 18. Iterators And Comparators/LibraryBooks/Program.cs
+++ 18. Iterators And Comparators/LibraryBooks/Program.cs
@@ -4,13 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book("title1", -2001);
+            Book book1 = new Book("title1", 2001);
             Book book2 = new Book("title2", 2002, "author 1");
             Book book3 = new Book("title3", 2003, "author 1", "author 2");
 
-            Console.WriteLine(book1);
-            Console.WriteLine(book2);
-            Console.WriteLine(book3);
+            Library library = new Library(book1, book2, book3);
+
+            foreach (var book in library)
+            {
+                Console.WriteLine(book);
+            }
         }
     }
 }
