3112d5635cf018f844383a7bfc3d7e5cf9ece9d9
bopzen <boppp@abv.bg>
2024-12-06 12:30:14 +0200

    Added Book class

---
+++ 18. Iterators And Comparators/LibraryBooks/Book.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBooks
+{
+    public class Book
+    {
+        public string Title { get; set; }
+        public int Year { get; set; }
+        public List<string> Authors {  get; private set; }
+
+        public Book(string title, int year, params string[] authors) 
+        {
+            if (string.IsNullOrWhiteSpace(title))
+            {
+                throw new ArgumentNullException("Title cannot be null or empty");
+            }
+
+            if (year < 0)
+            {
+                throw new ArgumentOutOfRangeException("Year cannot be negative");
+            }
+
+            Title = title;
+            Year = year;
+            Authors = new List<string>(authors);
+        }
+
+        public override string ToString()
+        {
+            string authors = Authors.Count > 0 ? string.Join(", ", Authors) : "No authors";
+            return $"Title: {Title}, Year: {Year}, Authors: {authors}";
+        }
+    }
+}
--- 18. Iterators And Comparators/LibraryBooks/Program.cs
+++ 18. Iterators And Comparators/LibraryBooks/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            
+            Book book1 = new Book("title1", -2001);
+            Book book2 = new Book("title2", 2002, "author 1");
+            Book book3 = new Book("title3", 2003, "author 1", "author 2");
+
+            Console.WriteLine(book1);
+            Console.WriteLine(book2);
+            Console.WriteLine(book3);
         }
     }
 }
