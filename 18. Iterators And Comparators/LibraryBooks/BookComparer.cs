2269c2cd51c457ebbf05bd70ed3996924493392a
bopzen <boppp@abv.bg>
2024-12-09 10:10:00 +0200

    Added more solutions

---
+++ 18. Iterators And Comparators/LibraryBooks/BookComparer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryBooks
+{
+    public class BookComparer : IComparer<Book>
+    {
+        public int Compare(Book? x, Book? y)
+        {
+            int result = x.CompareTo(y);
+            if (result == 0)
+            {
+                result = x.CompareTo(y);
+            }
+            return result;
+        }
+    }
+}
--- 18. Iterators And Comparators/LibraryBooks/Program.cs
+++ 18. Iterators And Comparators/LibraryBooks/Program.cs
@@ -1,16 +1,27 @@
-﻿namespace LibraryBooks
+﻿using System.Collections.Immutable;
+using System.Net.Http.Headers;
+
+namespace LibraryBooks
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Book book1 = new Book("title1", 2001);
-            Book book2 = new Book("title2", 2002, "author 1");
-            Book book3 = new Book("title3", 2003, "author 1", "author 2");
+            Book book1 = new Book("b title1", 2001);
+            Book book2 = new Book("c title2", 2002, "author 1");
+            Book book3 = new Book("a title3", 2003, "author 1", "author 2");
 
             Library library = new Library(book1, book2, book3);
+            List<Book> books = new List<Book> { book1, book2, book3 };
+            var bookComparator = new BookComparer();
+            books.Sort(bookComparator);
 
             foreach (var book in library)
+            {
+                Console.WriteLine(book);
+            }
+
+            foreach (var book in books)
             {
                 Console.WriteLine(book);
             }
